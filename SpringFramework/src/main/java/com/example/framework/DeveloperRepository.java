ac9e3df5854aa52044f55bb24eaf67ae3ce94089
Lahari276 <laharireddy147@gmail.com>
2021-05-10 11:35:30 +0530

    framework

---
+++ SpringFramework/src/main/java/com/example/framework/DeveloperRepository.java
@@ -0,0 +1,10 @@
+package com.example.framework;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+
+	
+}
